c70652e8f845b0e1d03997e35aa1494088090099
Torrie Fischer <tdfischer@hackerbots.net>
2015-10-08 00:19:01 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * Telekinesis - A UPnP ControlPoint
+ * Copyright (C) 2015 Torrie Fischer <tdfischer@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <QtGui/QApplication>
+#include <QtGui/QMainWindow>
+#include <QtGui/QLabel>
+
+int main(int argc, char** argv)
+{
+  QApplication app(argc, argv);
+  QMainWindow mw;
+  QLabel label("Telekinesis!", &mw);
+  mw.setCentralWidget(&label);
+  mw.show();
+
+  return app.exec();
+}
