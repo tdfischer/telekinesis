a0c4ea4621912cf939c737ab99ca9c7cc924f20f
Torrie Fischer <tdfischer@hackerbots.net>
2015-11-28 22:11:50 -0800

    util: implement a DeleteLaterPtr

--- src/util.h
+++ src/util.h
@@ -22,6 +22,15 @@
 #include <gobject/gobject.h>
 #include <memory>
 
+struct DeleteLaterDeleter {
+  template<typename T>
+    inline void
+    operator()(T *p)
+    {
+      if (p) p->deleteLater();
+    }
+};
+
 struct GObjectDeleter {
   template<typename T>
     inline void
@@ -35,6 +44,15 @@ struct GObjectDeleter {
     {
       g_object_ref_sink(p);
     }
+};
+
+template<typename T>
+class DeleteLaterPtr: public std::unique_ptr<T, DeleteLaterDeleter> {
+  public:
+    DeleteLaterPtr()
+      : std::unique_ptr<T, DeleteLaterDeleter>() {}
+    DeleteLaterPtr(T *p)
+      : std::unique_ptr<T, DeleteLaterDeleter>(p) {}
 };
 
 template<typename T, typename D>
